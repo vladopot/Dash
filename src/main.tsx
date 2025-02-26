aad45a34fac0f45e973033b5330f62baf067db5b
vladopot <vladsopot@mail.ru>
2025-02-26 15:11:33 +0100

    fix router

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { store } from './Redux/store.ts'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Dash">
         <App />
       </BrowserRouter>
     </Provider>
